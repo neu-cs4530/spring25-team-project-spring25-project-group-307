39d5c78ba3145b852439844644407fd0ca7587b5
vidyut-ramanan <98667881+vidyut-ramanan@users.noreply.github.com>
2025-03-21 02:25:56 -0400

    Embedded preview (#15)
    * detect if a message has a question link
    
    * Added clickable functionality to embeded card
    
    * linter fix

---
+++ client/src/components/main/messageCard/embededQuestionView/index.tsx
@@ -0,0 +1,64 @@
+import { Typography, Card, CardContent, Divider } from '@mui/material';
+import { PopulatedDatabaseQuestion } from '@fake-stack-overflow/shared';
+import { useNavigate } from 'react-router-dom';
+
+const EmbededQuestionView = ({ question }: { question: PopulatedDatabaseQuestion }) => {
+  const { title, text, askedBy, askDateTime, answers, views, upVotes, downVotes, comments } =
+    question;
+  const navigate = useNavigate();
+
+  return (
+    <Card
+      onClick={() => {
+        navigate(`/question/${question._id}`);
+      }}
+      variant='outlined'
+      sx={{
+        'margin': '20px auto',
+        'cursor': 'pointer',
+        'transition': 'background-color 0.3s ease',
+        '&:hover': { backgroundColor: '#f5f5f5' },
+      }}>
+      <CardContent>
+        <Typography variant='h5' component='div' gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant='body2' color='text.secondary' paragraph>
+          {text}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary' gutterBottom>
+          <strong>Asked By:</strong> {askedBy}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary' gutterBottom>
+          <strong>Asked On:</strong> {new Date(askDateTime).toDateString()}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary' gutterBottom>
+          <strong>Answers:</strong> {answers.length}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary' gutterBottom>
+          <strong>Views:</strong> {views.length}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary' gutterBottom>
+          <strong>Upvotes:</strong> {upVotes.length}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary' gutterBottom>
+          <strong>Downvotes:</strong> {downVotes.length}
+        </Typography>
+
+        <Divider sx={{ my: 2 }} />
+
+        <Typography variant='h6' gutterBottom>
+          Comments: {comments.length}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default EmbededQuestionView;
--- client/src/components/main/messageCard/index.tsx
+++ client/src/components/main/messageCard/index.tsx
@@ -1,21 +1,65 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './index.css';
-import { DatabaseMessage } from '../../../types/types';
+import { DatabaseMessage, PopulatedDatabaseQuestion } from '../../../types/types';
 import { getMetaData } from '../../../tool';
+import { getQuestionById } from '../../../services/questionService';
+import useUserContext from '../../../hooks/useUserContext';
+import EmbededQuestionView from './embededQuestionView';
 
 /**
  * MessageCard component displays a single message with its sender and timestamp.
  *
  * @param message: The message object to display.
  */
-const MessageCard = ({ message }: { message: DatabaseMessage }) => (
-  <div className='message'>
-    <div className='message-header'>
-      <div className='message-sender'>{message.msgFrom}</div>
-      <div className='message-time'>{getMetaData(new Date(message.msgDateTime))}</div>
+
+const MessageCard = ({ message }: { message: DatabaseMessage }) => {
+  const [question, setQuestion] = useState<PopulatedDatabaseQuestion | null>(null);
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    const isValidUrl = (input: string) => {
+      try {
+        const url = new URL(input);
+        return url;
+      } catch {
+        return undefined;
+      }
+    };
+
+    const baseUrl = new URL(window.location.origin);
+    const maybeUrl = isValidUrl(message.msg);
+
+    if (maybeUrl) {
+      if (
+        maybeUrl &&
+        baseUrl.hostname === maybeUrl.hostname &&
+        maybeUrl.pathname.startsWith('/question')
+      ) {
+        // remove leading and trailing slash and put the url parts in an array
+        const parts = maybeUrl.pathname.replace(/\/$/, '').split('/');
+        parts.shift();
+        if (parts.length === 2 && parts[0] === 'question') {
+          getQuestionById(parts[1], user.username)
+            .then(res => setQuestion(res || null))
+            .catch(() => setQuestion(null));
+        }
+      }
+    }
+  }, [message.msg, user.username]);
+
+  return (
+    <div className='message'>
+      <div className='message-header'>
+        <div className='message-sender'>{message.msgFrom}</div>
+        <div className='message-time'>{getMetaData(new Date(message.msgDateTime))}</div>
+      </div>
+      {question ? (
+        <EmbededQuestionView question={question} />
+      ) : (
+        <div className='message-body'>{message.msg}</div>
+      )}
     </div>
-    <div className='message-body'>{message.msg}</div>
-  </div>
-);
+  );
+};
 
 export default MessageCard;
