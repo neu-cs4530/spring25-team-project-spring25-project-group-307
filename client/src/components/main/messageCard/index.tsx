e8219d87649e5289991bf4a6c0102400b75f43c3
vidyut-ramanan <98667881+vidyut-ramanan@users.noreply.github.com>
2025-03-21 01:05:26 -0400

    detect if a message has a question link

--- client/src/components/main/messageCard/index.tsx
+++ client/src/components/main/messageCard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './index.css';
 import { DatabaseMessage } from '../../../types/types';
 import { getMetaData } from '../../../tool';
@@ -8,14 +8,49 @@ import { getMetaData } from '../../../tool';
  *
  * @param message: The message object to display.
  */
-const MessageCard = ({ message }: { message: DatabaseMessage }) => (
-  <div className='message'>
-    <div className='message-header'>
-      <div className='message-sender'>{message.msgFrom}</div>
-      <div className='message-time'>{getMetaData(new Date(message.msgDateTime))}</div>
+
+const MessageCard = ({ message }: { message: DatabaseMessage }) => {
+  const [questionId, setQuestionId] = useState('');
+
+  useEffect(() => {
+    const isValidUrl = (input: string) => {
+      try {
+        const url = new URL(input);
+        return url;
+      } catch {
+        return undefined;
+      }
+    };
+
+    const baseUrl = new URL(window.location.origin);
+    const maybeUrl = isValidUrl(message.msg);
+
+    if (maybeUrl) {
+      if (
+        maybeUrl &&
+        baseUrl.hostname === maybeUrl.hostname &&
+        maybeUrl.pathname.startsWith('/question')
+      ) {
+        // remove leading and trailing slash and put the url parts in an array
+        const parts = maybeUrl.pathname.replace(/\/$/, '').split('/');
+        parts.shift();
+        if (parts.length === 2 && parts[0] === 'question') {
+          setQuestionId(parts[1]);
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <div className='message'>
+      <div className='message-header'>
+        <div className='message-sender'>{message.msgFrom}</div>
+        <div className='message-time'>{getMetaData(new Date(message.msgDateTime))}</div>
+      </div>
+      <div className='message-body'>{message.msg}</div>
+      {questionId && 'this is a question'}
     </div>
-    <div className='message-body'>{message.msg}</div>
-  </div>
-);
+  );
+};
 
 export default MessageCard;
