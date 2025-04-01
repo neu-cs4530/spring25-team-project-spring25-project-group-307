55cba1310f55ba4a687ac624c07fa7054d6cc1cd
Melissa Jiang <melissajiang3@gmail.com>
2025-04-01 18:20:00 -0400

    improve sign in and sign up frontend components

--- client/src/components/auth/signup/index.tsx
+++ client/src/components/auth/signup/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './index.css';
 import { Link } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid2,
+  InputAdornment,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { Pets, Visibility, VisibilityOff } from '@mui/icons-material';
 import useAuth from '../../../hooks/useAuth';
 
 /**
@@ -13,62 +24,143 @@ const Signup = () => {
     password,
     passwordConfirmation,
     showPassword,
+    showPasswordConfirmation,
     err,
     handleSubmit,
     handleInputChange,
     togglePasswordVisibility,
+    togglePasswordConfirmationVisibility,
   } = useAuth('signup');
+  const paperStyle = {
+    'padding': '33px',
+    'width': '480px',
+    'margin': '20px auto',
+    'border-radius': '10px',
+  };
+  const btnStyle = { 'padding': '10px 14px', 'border-radius': '10px' };
+  const textFieldStyle = {
+    'marginTop': '8px',
+    '& .MuiOutlinedInput-root': {
+      borderRadius: '10px',
+    },
+  };
+  const errBoxSyle = {
+    border: '1px solid red',
+    borderRadius: '5px',
+    padding: '5px',
+    backgroundColor: '#f8d7da',
+  };
 
   return (
-    <div className='container'>
-      <h2>Sign up for FakeStackOverflow!</h2>
-      <form onSubmit={handleSubmit}>
-        <h4>Please enter your username.</h4>
-        <input
-          type='text'
-          value={username}
-          onChange={event => handleInputChange(event, 'username')}
-          placeholder='Enter your username'
-          required
-          className='input-text'
-          id='username-input'
-        />
-        <h4>Please enter your password.</h4>
-        <input
-          type={showPassword ? 'text' : 'password'}
-          value={password}
-          onChange={event => handleInputChange(event, 'password')}
-          placeholder='Enter your password'
-          required
-          className='input-text'
-          id='password-input'
-        />
-        <input
-          type={showPassword ? 'text' : 'password'}
-          value={passwordConfirmation}
-          onChange={e => handleInputChange(e, 'confirmPassword')}
-          placeholder='Confirm your password'
-          required
-          className='input-text'
-        />
-        <div className='show-password'>
-          <input
-            type='checkbox'
-            id='showPasswordToggle'
-            checked={showPassword}
-            onChange={togglePasswordVisibility}
-          />
-          <label htmlFor='showPasswordToggle'>Show Password</label>
-        </div>
-        <button type='submit' className='login-button'>
-          Submit
-        </button>
-      </form>
-      {err && <p className='error-message'>{err}</p>}
-      <Link to='/' className='login-link'>
-        Have an account? Login here.
-      </Link>
-    </div>
+    <Grid2
+      container
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Paper elevation={10} sx={paperStyle}>
+        <FormControl onSubmit={handleSubmit} component='form' noValidate>
+          <Typography
+            variant='h4'
+            sx={{
+              padding: '4px 0px',
+              textAlign: 'center',
+              color: 'primary.main',
+            }}>
+            HuskyFlow <Pets />
+          </Typography>
+          <Typography variant='h5' sx={{ paddingBottom: '8px' }}>
+            Create new account
+          </Typography>
+          <div>
+            <label htmlFor='username'>
+              Username<span style={{ color: 'red' }}>*</span>
+            </label>
+            <TextField
+              sx={textFieldStyle}
+              id='username'
+              value={username}
+              placeholder='Enter username'
+              variant='outlined'
+              onChange={event => handleInputChange(event, 'username')}
+              fullWidth
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>
+              Password<span style={{ color: 'red' }}>*</span>
+            </label>
+            <TextField
+              sx={textFieldStyle}
+              id='password'
+              value={password}
+              placeholder='Enter password'
+              type={showPassword ? 'text' : 'password'}
+              variant='outlined'
+              onChange={event => handleInputChange(event, 'password')}
+              fullWidth
+              required
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <Button onClick={togglePasswordVisibility}>
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </Button>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor='confirmPassword'>
+              Confirm Password<span style={{ color: 'red' }}>*</span>
+            </label>
+            <TextField
+              sx={textFieldStyle}
+              id='confirmPassword'
+              value={passwordConfirmation}
+              placeholder='Enter password'
+              type={showPasswordConfirmation ? 'text' : 'password'}
+              variant='outlined'
+              onChange={event => handleInputChange(event, 'confirmPassword')}
+              fullWidth
+              required
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <Button onClick={togglePasswordConfirmationVisibility}>
+                      {showPasswordConfirmation ? <Visibility /> : <VisibilityOff />}
+                    </Button>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Typography variant='body2' sx={{ marginTop: '8px', float: 'right', color: 'red' }}>
+              *required fields
+            </Typography>
+          </div>
+          {err && (
+            <Box sx={errBoxSyle}>
+              <Typography sx={{ color: 'red', textAlign: 'center' }}>{err}</Typography>
+            </Box>
+          )}
+          <Button type='submit' color='primary' variant='contained' sx={btnStyle} fullWidth>
+            Sign up
+          </Button>
+          <Typography sx={{ textAlign: 'center' }}>
+            Already have an account?{' '}
+            <Link to='/' style={{ textDecoration: 'none' }}>
+              <Typography component='span' sx={{ color: 'primary.main', fontWeight: 'bold' }}>
+                Sign in
+              </Typography>
+            </Link>
+          </Typography>
+        </FormControl>
+      </Paper>
+    </Grid2>
   );
 };
 
